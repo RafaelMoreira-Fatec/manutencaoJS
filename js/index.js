515720121363300838c9e04636cade066127a1b0
Rafael <rafaelmoreira0303@gmail.com>
2026-01-03 00:48:31 -0300

    estrutura inicial criada

---
+++ js/index.js
@@ -0,0 +1,63 @@
+init()
+
+function init() {
+
+    selectCard()
+
+    const button = document.querySelector('#new-solicitation form button')
+    const dateField = document.querySelector('#new-solicitation input[type=date]');
+    const currentyDate = new Date();
+    let day = currentyDate.getDate().toString();
+    let mounth = (currentyDate.getMonth() + 1).toString();
+    const fullYear = currentyDate.getFullYear();
+
+    console.log(day.length)
+    console.log(day)
+    day = day.length == 1 ? "0".concat(day) : day;
+    mounth = mounth.length == 1 ? "0".concat(mounth) : mounth;
+
+    console.log(day)
+
+    dateField.value = `${fullYear}-${mounth}-${day}`
+
+    button.addEventListener('click', e => {
+        console.log('teste')
+        let form = document.querySelector('#new-solicitation form')
+
+        for (i = 0; i < (form.length - 1); i++) {
+            console.log(form[i])
+        }
+    })
+}
+
+
+function selectCard() {
+
+    const sectionCards = [...document.querySelectorAll('.section-card')]
+
+    const tabs = [...document.querySelectorAll("li")]
+    const tabsObeject = {}
+
+    let currentyTab;
+
+    console.log(tabs)
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', e => {
+            currentyTab = tab.dataset.tab;
+
+            tab.classList.add('active')
+
+            sectionCards.forEach((card, i) => {
+                currentyTab
+                if (currentyTab == card.dataset.section) {
+                    card.classList.remove('hide-section');
+                } else {
+                    card.classList.add('hide-section');
+                    tabs[i].classList.remove('active')
+                }
+            })
+        })
+    })
+
+}
