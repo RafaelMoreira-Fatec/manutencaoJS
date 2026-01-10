5684b447a581ca1362256f11f10e4e69191df431
Rafael <rafaelmoreira0303@gmail.com>
2026-01-10 20:48:14 -0300

    eventos

--- react/manutencaor-react/src/components/Funcionario.jsx
+++ react/manutencaor-react/src/components/Funcionario.jsx
@@ -1,8 +1,16 @@
 function Funcionario({nome, setor = "produção", salario = 1500, children, ...rest}){
+    
+    const dizerOi = (mensagem, event) => {
+        alert(mensagem)
+        console.log(mensagem, event)
+    }
 
   return(
+
+
     <>
     <p>Meu nome é {nome} -- trabalho no setor de {setor} -- salario R${salario} {rest.entrada && ' -- Entrada : ' + rest.entrada }</p>
+    <button onClick={(event) => dizerOi('meu nome é ' + nome, event)}>dizer oi</button>
     <p>{children}</p>
     </>
   )
